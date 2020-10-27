316dfd58859834dce306d246ce02295c80fb1330
McKayla Lankau <lankaukk@yahoo.com>
2020-10-27 00:25:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,6 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    #@name = student_hash[:name]
     self.send("name=", student_hash[:name])
     self.send("location=", student_hash[:location])
     self.send("profile_url=", student_hash[:profile_url])
