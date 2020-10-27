1ba4dd8f9a35844b065a76028c54d67aa83d49b1
McKayla Lankau <lankaukk@yahoo.com>
2020-10-27 00:10:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,11 +5,24 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
-  end
+    student = {}
+    profile_page = Nokogiri::HTML(open(profile_url))
+    links = profile_page.css(".social-icon-container").children.css("a").map {|el| el.attribute('href').value }
+    links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      else 
+        student[:blog] = link
+      end
+    end
 
 end
 
